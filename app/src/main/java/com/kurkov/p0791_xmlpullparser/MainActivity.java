1c24effae8e28288d50cc9733b38e91d367087e9
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2015-09-22 10:21:58 +0300

    Initial commit

---
+++ app/src/main/java/com/kurkov/p0791_xmlpullparser/MainActivity.java
@@ -0,0 +1,88 @@
+package com.kurkov.p0791_xmlpullparser;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+public class MainActivity extends Activity {
+
+    final String LOG_TAG = "myLogs";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String tmp = "";
+
+        try {
+            XmlPullParser xpp = prepareXpp2();
+            while (xpp.getEventType() != XmlPullParser.END_DOCUMENT) {
+                switch (xpp.getEventType()) {
+                    // начало документа
+                    case XmlPullParser.START_DOCUMENT:
+                        Log.d(LOG_TAG, "START_DOCUMENT");
+                        break;
+                    // начало тэга
+                    case XmlPullParser.START_TAG:
+                        Log.d(LOG_TAG, "START_TAG: name = " + xpp.getName()
+                                + ", depth = " + xpp.getDepth() + ", attrCount = "
+                                + xpp.getAttributeCount());
+                        tmp = "";
+                        for (int i = 0; i < xpp.getAttributeCount(); i++) {
+                            tmp = tmp + xpp.getAttributeName(i) + " = "
+                                    + xpp.getAttributeValue(i) + ", ";
+                        }
+                        if (!TextUtils.isEmpty(tmp))
+                            Log.d(LOG_TAG, "Attributes: " + tmp);
+                        break;
+                    // конец тэга
+                    case XmlPullParser.END_TAG:
+                        Log.d(LOG_TAG, "END_TAG: name = " + xpp.getName());
+                        break;
+                    // содержимое тэга
+                    case XmlPullParser.TEXT:
+                        Log.d(LOG_TAG, "text = " + xpp.getText());
+                        break;
+
+                    default:
+                        break;
+                }
+                // следующий элемент
+                xpp.next();
+            }
+            Log.d(LOG_TAG, "END_DOCUMENT");
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // читаем xml из файла
+    XmlPullParser prepareXpp() {
+        return getResources().getXml(R.xml.data);
+    }
+
+    // читаем поток из xml-строки
+    XmlPullParser prepareXpp2() throws XmlPullParserException {
+        // получаем фабрику
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        // включаем поддержку namespace (по умолчанию выключена)
+        factory.setNamespaceAware(true);
+        // создаем парсер
+        XmlPullParser xpp = factory.newPullParser();
+        // даем парсеру на вход Reader
+        xpp.setInput(new StringReader(
+                "<data><phone><company>Samsung</company></phone></data>"));
+        return xpp;
+    }
+}
